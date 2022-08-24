afff01576256d8d21318769a6ef3b73fcab9ab83
James Lee <j.lee1203@hotmail.com>
2022-08-24 10:50:11 -0700

    commit tree 0f3e78df5e681ff62d6cf7a2c0060f7cd40a55dd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cardRecipients = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cardRecipients, surprise){
+const shoppingCart = [];    
+    for(let i = 0; i < cardRecipients.length; i++){
+        console.log(`Thank you, ${cardRecipients[i]}, for the wonderful ${surprise} gift!`);
+        shoppingCart.push(`Thank you, ${cardRecipients[i]}, for the wonderful ${surprise} gift!`);
+    }    
+return shoppingCart;
+}
+
+function countDown(){
+let counter = 10;
+while (counter >= 0){
+    console.log(counter--);
+    }
+}
